1c68a0a5b029f5aab00b6e8afe8628e811bd5207
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-04-21 16:55:26 -0700

    Added

--- src/main/java/com/cloudmersive/Main.java
+++ src/main/java/com/cloudmersive/Main.java
@@ -1,10 +1,9 @@
 package com.cloudmersive;
 
 import com.cloudmersive.client.*;
-import com.cloudmersive.client.invoker.auth.ApiKeyAuth;
 import com.cloudmersive.client.model.*;
 import com.cloudmersive.client.invoker.*;
-import org.springframework.web.client.RestClientException;
+import java.io.*;
 
 public class Main {
 
@@ -14,16 +13,58 @@ public class Main {
 
 
 
-        EmailApi api = new EmailApi(client);
+//        ApiKeyAuth auth = (ApiKeyAuth)client.getAuthentication("key");
+//        auth.setApiKey("14343416-776b-40e8-b6d7-6ffaa0bd70f9");
+
+//        ApiKeyAuth auth = new ApiKeyAuth("header", "Apikey");
+//        auth.setApiKey("14343416-776b-40e8-b6d7-6ffaa0bd70f9");
+//
+//        client.getAuthentications().put("Apikey", auth);
+
+        //client.setApiKey("14343416-776b-40e8-b6d7-6ffaa0bd70f9");
+
+
+
 
         try
         {
+            // Email API
+
+            EmailApi api = new EmailApi(client);
+
+
             FullEmailValidationResponse result = api.emailFullValidation("support@cloudmersive.com");
-            System.out.println("Result: " + result.getValidAddress().toString() + " - " + result.getMailServerUsedForValidation());
+            System.out.println("Result: " + result.isValidAddress().toString() + " - " + result.getMailServerUsedForValidation());
+
+            // Barcode API
+
+            BarcodeLookupApi barcodeLookup = new BarcodeLookupApi(client);
+
+            BarcodeLookupResponse result2 = barcodeLookup.barcodeLookupEanLookup("4011200296908");
+            System.out.println(result2.toString());
+
+            GenerateBarcodeApi generate = new GenerateBarcodeApi(client);
+            generate.generateBarcodeEAN8("96385074");
+
+            // Convert API
+
+            ConvertDocumentApi docApi = new ConvertDocumentApi(client);
+
+            ScreenshotRequest req = new ScreenshotRequest();
+            req.setUrl("https://api.cloudmersive.com/csharp-client.asp");
+
+            ConvertWebApi webApi = new ConvertWebApi(client);
+            File output = webApi.convertWebUrlToPdf(req);
+
+            System.out.println(output.getPath());
         }
-        catch (RestClientException e)
+        catch (Exception e)
         {
             System.out.println("Error:" + e.toString() + e.getMessage() );
         }
+
+
+
+
     }
 }
